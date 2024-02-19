ee98baa57e0d62da9f0036ea2f0010bfb407c18b
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2024-02-19 22:03:19 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./_components/Hero";
 import ProductSection from "./_components/ProductSection";
-
+import { Suspense } from 'react';
 export default function Home() {
   return (
     <div className="">
+      <Suspense>
   <Hero/>
   <ProductSection/>
+  </Suspense>
     </div>
   );
 }
